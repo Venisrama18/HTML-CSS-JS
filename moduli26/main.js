ef5f740a846f0fd9a8bf137524f2f667e1019d7b
Venisrama18 <venisrama7@gmail.com>
2026-03-05 17:41:40 +0100

    moduli26

--- moduli26/main.js
+++ moduli26/main.js
@@ -1,1 +1,102 @@
 console.log('hello');
+
+var x=10;
+
+var y=20;
+
+var value = "Shkolla digjitale";
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+var a = 99;
+
+a++;
+
+console.log(a);
+
+
+
+var b = 100;
+
+b--;
+
+console.log(b);
+
+
+
+
+var c = 30;
+
+c+=10;
+
+console.log(c);
+
+
+
+
+var d = 65;
+
+d-=5;
+
+console.log(d);
+
+
+
+var e = 50;
+
+e*=6;
+
+console.log(e);
+
+
+
+
+var f = 100;
+
+f/=4;
+
+console.log(f);
+
+
+
+var g = 10;
+
+var h = 20;
+
+var result = g > h;
+
+console.log(result);
+
+
+var i = 15;
+
+var j = 10;
+
+var result2 = i < j;
+
+console.log(result2);
+
+
+var k = 5;
+
+var l = 5;
+
+var result3 = k >= l;
+
+console.log(result3);
+
+
+var emri = "Venis";
+var mbiemri = "Rama";
+
+var fullname = emri + " " + mbiemri;
+
+console.log(fullname);
+
+console.log(`Hello my name is ${emri}`);
