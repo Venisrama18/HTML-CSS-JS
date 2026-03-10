81a53ed3fa6545d49df41440256bf544d41a5471
Venisrama18 <venisrama7@gmail.com>
2026-03-10 17:43:07 +0100

    moduli27

---
+++ moduli27/main.js
@@ -0,0 +1,13 @@
+function displayAlert(){
+
+    alert("This is a image");
+}
+
+
+displayAlert();
+
+function mbledhja(numri1,numri2){
+        return numri1+numri2;
+}
+
+document.write(mbledhja(5,8));
