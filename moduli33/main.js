097c815a670d863feeb2ac351359e1360b00c912
Venisrama18 <venisrama7@gmail.com>
2026-03-26 17:44:00 +0100

    moduli33

---
+++ moduli33/main.js
@@ -0,0 +1,18 @@
+// var mosha=6
+
+// if(mosha<7){
+//     document.write("Femija juaj eshte me i vogel se 7 vjec")
+// }else{
+//     document.write("Ne do tju lajmrojm kur kemi ndonje grup per ate moshe ")
+// }
+
+
+var age=12
+
+if(age<7){
+    document.write("Femija juaj eshte me i vogel se 7 vjec")
+}else if(age>18){
+    document.write("Femija juaj eshte me i madh se 18 vjec")
+}else{
+    document.write("Ne do tju lajmrojm kur kemi ndonje grup per ate moshe ")
+}
