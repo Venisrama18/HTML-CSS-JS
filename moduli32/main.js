b7f425e5ef6b9803623cadd3ecd89de2e37b7409
Venisrama18 <venisrama7@gmail.com>
2026-03-26 17:16:57 +0100

    moduli32

--- moduli32/main.js
+++ moduli32/main.js
@@ -31,3 +31,56 @@ console.log(number);
 programmingLanguages.unshift("C#");
 
 console.log(programmingLanguages);
+
+// shift - delete the first element of the list
+
+programmingLanguages.shift();  
+
+console.log(programmingLanguages);
+
+// splice - cilin elemnt me hek ,cilen pozite ,edhe cilin me shtu
+
+programmingLanguages.splice(1, 2, "Ruby");
+
+console.log(programmingLanguages);
+
+
+
+var student = ['Redon', 'Patris', 'Ernes', 'Detjon'];
+
+var [s1, s2, s3, s4] = student;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+
+
+var qytetet = ['Prishtine', 'Peje', 'Gjakove', 'Prizren', 'Ferizaj', 'Gjilan', 'Mitrovice', 'Vushtrri', 'Podujeve', 'Deçan', 'Tirane', 'Shkup', 'Sarajevo', 'Beograd', 'Zagreb', 'Ljubljana', 'Skopje', 'Podgorica', 'Sofia', 'Athine',];
+
+var [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20,] = qytetet;
+
+console.log(q1);
+console.log(q2);
+console.log(q3);
+console.log(q4);
+console.log(q5);
+console.log(q6);
+console.log(q7);
+console.log(q8);
+console.log(q9);
+console.log(q10);
+console.log(q11);
+console.log(q12);
+console.log(q13);
+console.log(q14);
+console.log(q15);
+console.log(q16);
+console.log(q17);
+console.log(q18);
+console.log(q19);
+console.log(q20);
+
+
+
+
