1fd4279e47c95615e0435e6d3167256522331719
Venisrama18 <venisrama7@gmail.com>
2026-04-14 17:48:47 +0200

    lesson30

--- lesson30/DSclock/js/mian.js
+++ lesson30/DSclock/js/mian.js
@@ -3,21 +3,71 @@ var dstime;
 var sleeptime;
 var noon = 12;
 
+
+
+
 function showCurrentTime(){
     var clock = document.getElementById('clock');
-    var currentTime = new Date()
+    var currentTime = new Date();
+
 
     var hours = currentTime.getHours();
-    var minjutes = currentTime.getMinutes();
+    var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
 
+
     var meridian = "AM";
+
+
 
     if(hours >= noon){
         meridian = "PM";
     }
 
-    var clockTime = hours + ":" +minutes + ":" + seconds + "" + meridian;
-    clock.innerHTML= clockTime;
+
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian;
+    clock.innerText = clockTime;
     changeImage();
 };
+
+
+
+var oneSecond = 1000;
+setInterval(showCurrentTime, oneSecond);
+
+function changeImage(){
+    var time = new Date().getHours();
+
+    var image = "img/ds_clock.png"
+    var imageHTML = document.getElementById("timeImage");
+
+    if(time == wakeuptime){
+        image ="img/morning.gif";
+    }
+
+    else if(time == dstime){
+        image = "img/class.gif";
+    }
+
+    else if( time == sleeptime){
+        image = "img/night.gif";
+    }
+
+
+    imageHTML.src = image;
+
+}
+
+function updateClock(){
+    var wakeuptimeselector = document.getElementById("wakeuptimeselector");
+    wakeuptime = wakeuptimeselector.value
+
+    var dsTimeSelector = document.getElementById("dsTimeSelector");
+    dstime = dsTimeSelector.value;
+
+    var sleepTimeSelector = document.getElementById("sleepTimeSelector");
+    sleeptime = sleepTimeSelector.value;
+}
+
+var saveButton = document.getElementById("saveButton");
+saveButton.addEventListener("click", updateClock)
