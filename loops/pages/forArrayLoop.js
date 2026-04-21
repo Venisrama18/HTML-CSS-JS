4fa27eadd383a2d338059f8e3750f8ce4587deca
Venisrama18 <venisrama7@gmail.com>
2026-04-21 17:46:24 +0200

    loop

---
+++ loops/pages/forArrayLoop.js
@@ -0,0 +1,6 @@
+var names =["Steve","Bill","Mark"];
+var x;
+
+for(x of names){
+    document.getElementById("forOfArrayLoop").innerHTML += x +"<br>";
+}
---
+++ loops/pages/forOfStringLoop.js
@@ -0,0 +1,7 @@
+var text = "Javascript";
+
+var x;
+
+for(x of text){
+    document.getElementById("forOfStringLoop").innerHTML += x +"<br>"
+}
